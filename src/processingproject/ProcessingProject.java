34ba12cf717527736e8613e2862a5521e9b8f62a
Adit Rajaram <rajitadarth422@gmail.com>
2024-07-23 11:21:43 -0500

    commit

---
+++ src/processingproject/ProcessingProject.java
@@ -0,0 +1,18 @@
+package processingproject;
+import processing.core.PApplet;
+public class ProcessingProject extends PApplet {
+    public static void main(String[] args){
+        PApplet.main("processingproject.ProcessingProject");
+    }
+    public void settings(){
+        size(500,500);
+    }
+
+    public void setup(){
+        background(234, 55, 255);
+    }
+
+    public void draw(){
+
+    }
+}
